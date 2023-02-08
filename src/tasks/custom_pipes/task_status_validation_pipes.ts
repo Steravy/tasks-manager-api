c2fe7b50d049b1f836c0e77440019d18061ef79d
Stefan Vitoria <steph.74lw@gmail.com>
2023-02-08 19:54:20 -0100

    implementation of TaskStatusValidationPipe in progress

--- src/tasks/custom_pipes/task_status_validation_pipes.ts
+++ src/tasks/custom_pipes/task_status_validation_pipes.ts
@@ -1,8 +1,14 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
+import { TaskStatus } from "../tasks.model";
 
 
 @Injectable()
 export class TaskStatusValidationPipe implements PipeTransform {
+    readonly validStatus = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE,
+    ]
 
   transform(value: any) {
 
