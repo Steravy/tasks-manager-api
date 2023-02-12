6aba8d99d650f2dfd1d7c16ec87e1fc2726fc9ea
Stefan Vitoria <steph.74lw@gmail.com>
2023-02-12 21:10:33 -0100

    done typeOrm configurations to connect to db

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: 'Programerfor22',
       database: 'taskmanagement',
       entities: [__dirname + '/../**/*.entity.ts'],
       synchronize: true,
